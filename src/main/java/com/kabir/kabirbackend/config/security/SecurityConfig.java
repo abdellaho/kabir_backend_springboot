eb2b7b20d5f151c3566701e8c0bbe65cfc552e38
Abdellah <bojaneabdellaho@gmail.com>
2026-01-05 23:48:41 +0100

    edit endpoints

--- src/main/java/com/kabir/kabirbackend/config/security/SecurityConfig.java
+++ src/main/java/com/kabir/kabirbackend/config/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.kabir.kabirbackend.config.security;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -19,11 +20,15 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins;
 
     @Autowired
     private JwtFilter jwtFilter;
@@ -77,10 +82,14 @@ public class SecurityConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
+        List<String> allowedOriginsList = Arrays.stream(allowedOrigins.split(","))
+                .map(String::trim)
+                .toList();
+
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOriginPatterns(List.of("http://localhost:4200"));
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOriginPatterns(allowedOriginsList);
+        config.setAllowedOrigins(allowedOriginsList);
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
